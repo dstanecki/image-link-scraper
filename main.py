e098cd811d4a38b05b51d9d9a387772c2e83b16a
Daniel Stanecki <dstanecki@hawk.iit.edu>
2022-03-05 13:25:11 -0600

    Cleaned up code

--- main.py
+++ main.py
@@ -1,43 +1,29 @@
 import urllib
+import requests
 from pandas import *
 import pandas as pd
 import urllib.request
-import mailchimp_marketing as MailchimpMarketing
-from mailchimp_marketing.api_client import ApiClientError
 
 
-def scrape():
-    csv_name = input("Enter the spreadsheet file name:\n") # change this to method parameter^
+if __name__ == '__main__':
+    csv_name = input("Enter the spreadsheet file name:\n")
     data = read_csv(csv_name)
-    images_unfiltered = data["Featured Image"].tolist()
-    images = [x for x in images_unfiltered if pd.isnull(x) == False]
-    count = 1
+    images_unfiltered = data["Featured Image"].tolist()  # Puts all cell values in "Featured Image" column into a list
+    images = [x for x in images_unfiltered if pd.isnull(x) == False]  # Filters out blank cell values. Remaining values should all be direct URL links to images 
+    scrape()
+    
+    
+# Iterates through 'images' list and saves images to a local directory
+def scrape():
     for url in images:
         try:
-            file_name = '/Users/Library/Desktop/Test/' + url.rsplit('/', 1)[-1] # names the file whatever is after
-            # the final slash in the URL
+            file_name = '/Users/Library/Desktop/eNewsletter/' + url.rsplit('/', 1)[-1] # Saves images to eNewsletter folder, 
+            # file is named after the text that follows the last slash in the url
             req = urllib.request.Request(url, headers={
                 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
                 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'})
             with urllib.request.urlopen(req) as response, open(file_name, 'wb') as out_file:
-                data = response.read()  # a `bytes` object
+                data = response.read()
                 out_file.write(data)
-            count += 1
         except Exception as exc:
             print(f"Exception occurred while downloading image from url {url} {str(exc)}")
-    # still want to use Mailchimp API to add files to File Manager, replicate campaign
-
-def upload():
-    try:
-        client = MailchimpMarketing.Client()
-        client.set_config({
-            "api_key": "YOUR_API_KEY",
-            "server": "YOUR_SERVER_PREFIX"
-        })
-        response = client.fileManager.upload({"name": "name", "file_data": "file_data"})
-        print(response)
-    except ApiClientError as error:
-        print("Error: {}".format(error.text))
-
-if __name__ == '__main__':
-    scrape()
